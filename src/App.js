14fc14c73783dc051f19d832a62ed814cc1fd320
jacobcurry <jacob.a.curry@gmail.com>
2022-12-10 11:14:40 -0500

    added quiz form

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ import NewUserForm from "./components/NewUserForm";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 import Quiz from "./components/Quiz";
+import Navbar from "./components/Navbar";
 
 import "./App.css";
 
 const App = () => {
   const [currentUser, setCurrentUser] = useState({});
+
+  axios.defaults.headers.common["Content-Type"] =
+    "application/json;charset=utf-8";
+  axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 
   const axiosInstance = axios.create({
     baseURL: "https://lit-anchorage-15647.herokuapp.com/",
@@ -18,46 +23,44 @@ const App = () => {
   });
 
   const handleCreateUser = async (userObj) => {
-    const response = await axiosInstance.post("createaccount", userObj, {
-      withCredentials: true,
-    });
-    console.log(response);
-    // axios
-    //   .post(
-    //     "https://lit-anchorage-15647.herokuapp.com/createaccount",
-    //     userObj,
-    //     { withCredentials: true }
-    //   )
-    //   .then((response) => {
-    //     console.log(response);
-    // if (response.data) {
-    //   console.log("hi");
-    //   console.log(response);
-    //   setCurrentUser(response.data);
-    //   handleToggleLogout();
-    // }
-    //});
+    // const response = await axiosInstance.post("createaccount", userObj, {
+    //   withCredentials: true,
+    // });
+    // console.log(response);
+    axios
+      .post(
+        "https://lit-anchorage-15647.herokuapp.com/createaccount",
+        userObj,
+        { withCredentials: true }
+      )
+      .then((response) => {
+        console.log(response);
+        if (response.data) {
+          console.log("hi");
+          console.log(response);
+          setCurrentUser(response.data);
+        }
+      });
   };
 
   const handleLogin = async (userObj) => {
-    const response = await axiosInstance.put("login", userObj, {
-      withCredentials: true,
-    });
-    console.log(response);
-    // console.log(userObj);
-    // axios
-    //   .put("https://lit-anchorage-15647.herokuapp.com/login", userObj, {
+    //   const response = await axiosInstance.put("login", userObj, {
     //     withCredentials: true,
-    //   })
-    //   .then((response) => {
-    //     console.log(response);
-    //     if (response.data.user) {
-    //       console.log("hi");
-    //       console.log(response);
-    //       setCurrentUser(response.data.user);
-    //       handleToggleLogout();
-    //     }
     //   });
+    //   console.log(response);
+    // console.log(userObj);
+    axios
+      .put("https://lit-anchorage-15647.herokuapp.com/login", userObj, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        console.log(response);
+        if (response.data.user) {
+          console.log("hi");
+          console.log(response);
+          setCurrentUser(response.data.user);
+        }
+      });
   };
 
   useEffect(() => {
@@ -73,6 +76,7 @@ const App = () => {
   return (
     <Router>
       <div className="App">
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/quiz" element={<Quiz />} />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [toggleShowForm, setToggleShowForm] = useState(false);
+  const [quizData, setQuizData] = useState([]);
+  const [amount, setAmount] = useState(10);
+  const [category, setCategory] = useState("any");
+  const [difficulty, setDifficulty] = useState("any");
+
+  const axiosInstance = axios.create({
+    baseURL: `https://opentdb.com`,
+    header: { "Access-Control-Allow_Origin": "*" },
+  });
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    if (category === "any" && difficulty === "any") {
+      const response = await axiosInstance.get(`api.php?amount=${amount}`);
+      console.log(response);
+    } else if (difficulty !== "any" && category === "any") {
+      const response = await axiosInstance.get(
+        `api.php?amount=${amount}&difficulty=${difficulty}`
+      );
+      console.log(response);
+    } else if (category !== "any" && difficulty === "any") {
+      const response = await axiosInstance.get(
+        `api.php?amount=${amount}&category=${category}`
+      );
+      console.log(response);
+    } else {
+      const response = await axiosInstance.get(
+        `api.php?amount=${amount}&category=${category}&difficulty=${difficulty}`
+      );
+      console.log(response);
+    }
+  };
+
+  return (
+    <div className="">
+      <div className="">
+        <p className="">Choose from many different options to create a quiz!</p>
+        <button
+          onClick={() => setToggleShowForm(!toggleShowForm)}
+          className="btn"
+        >
+          {toggleShowForm ? "Hide Form" : "Get Started"}
+        </button>
+      </div>
+      {toggleShowForm ? (
+        <form onSubmit={handleFormSubmit}>
+          <label htmlFor="numQuestions">Number of Questions:</label>
+          <input
+            name="numQuestions"
+            type="number"
+            min={1}
+            max={50}
+            defaultValue={10}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+          <label htmlFor="category">Select a Category</label>
+          <select
+            onChange={(e) => {
+              setCategory(e.target.value);
+            }}
+            name="category"
+            className=""
+          >
+            <option value="any">Any Category</option>
+            <option value="9">General Knowledge</option>
+            <option value="10">Entertainment: Books</option>
+            <option value="11">Entertainment: Film</option>
+            <option value="12">Entertainment: Music</option>
+            <option value="13">Entertainment: Musicals &amp; Theatres</option>
+            <option value="14">Entertainment: Television</option>
+            <option value="15">Entertainment: Video Games</option>
+            <option value="16">Entertainment: Board Games</option>
+            <option value="17">Science &amp; Nature</option>
+            <option value="18">Science: Computers</option>
+            <option value="19">Science: Mathematics</option>
+            <option value="20">Mythology</option>
+            <option value="21">Sports</option>
+            <option value="22">Geography</option>
+            <option value="23">History</option>
+            <option value="24">Politics</option>
+            <option value="25">Art</option>
+            <option value="26">Celebrities</option>
+            <option value="27">Animals</option>
+            <option value="28">Vehicles</option>
+            <option value="29">Entertainment: Comics</option>
+            <option value="30">Science: Gadgets</option>
+            <option value="31">
+              Entertainment: Japanese Anime &amp; Manga
+            </option>
+            <option value="32">Entertainment: Cartoon &amp; Animations</option>{" "}
+          </select>
+          <label htmlFor="difficulty"></label>
+          <select
+            onChange={(e) => {
+              setDifficulty(e.target.value);
+            }}
+            name="difficulty"
+            className=""
+          >
+            <option value="any">Any Difficulty</option>
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+          </select>
+          <button className="btn" type="submit">
+            Take Quiz
+          </button>
+        </form>
+      ) : null}
+    </div>
+  );
 };
 
 export default Home;
