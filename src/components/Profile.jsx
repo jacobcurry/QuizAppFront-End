c7d00f048fc3c5f8a8229000168f8ad6d4b6e9bf
angelv18 <angelvasquez0918@gmail.com>
2022-12-12 11:58:16 -0500

    css

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,7 +7,9 @@ const Profile = (props) => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(null);
   const [showUser, setShowUser] = useState([]);
- 
+  const [updatedFirstName, setUpdatedFirstName] = useState();
+  const [updatedLastName, setUpdatedLastName] = useState();
+  const [updatedEmail, setUpdatedEmail] = useState()
 
   const handleLogout = () => {
     localStorage.removeItem("user");
@@ -78,6 +80,32 @@ const Profile = (props) => {
    
   };
 
+  const handleUpdatedProfileInfo = async () => {
+    setUpdatedFirstName();
+    setUpdatedLastName();
+    setUpdatedEmail();
+
+    const response = await fetch(
+      `https://lit-anchorage-15647.herokuapp.com/${props.currentUser.email}`,
+      {
+        method: "Put",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        firstname: updatedFirstName,
+        lastname: updatedLastName,
+        email: updatedEmail
+      }
+    );
+    const json = await response.json();
+    console.log(json);
+    setUpdatedFirstName(json)
+    setUpdatedLastName(json)
+    setUpdatedEmail(json)
+  }
+  
+
   //   if (!response.ok) {
   //     setIsLoading(false);
   //     setError(json.error);
@@ -106,7 +134,7 @@ const Profile = (props) => {
             >
               Profile Info
             </li>
-            <li className="nav-li">Update</li>
+            <li className="nav-li" onClick={handleUpdatedProfileInfo}>Update</li>
           </div>
           <div>
             <li onClick={handleLogout} className="nav-li">
@@ -132,7 +160,7 @@ const Profile = (props) => {
             </div>
           </div>
         ) : null}
-        {showProfileInfo ? <div className="profile-display">Email: {showUser.email}<br/>Last Name: {showUser.lastname}<br/>First Name: {showUser.firstname}</div> : null}
+        {showProfileInfo ? <div className="profile-display">First Name: {showUser.firstname}<br/>Last Name: {showUser.lastname}<br/><span>Email:</span> {showUser.email}<br/></div> : null}
       </div>
     </div>
   );
